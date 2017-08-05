d4ac6117510515b25944b910461f42e6b5cd49fb
wildskyf <poppin.wildsky@gmail.com>
2017-08-05 12:58:33 +0800

    add mode-switcher & credit

--- newtab.js
+++ newtab.js
@@ -1,7 +1,8 @@
-var VERSION = 1;
+var VERSION = 2;
 
 window.onload = () => {
   var $textarea = document.querySelector('#note-content');
+  var $mode_switcher = document.querySelector('#mode-switcher');
 
   var initNote = () => {
     browser.storage.sync.get().then( data => {
@@ -9,12 +10,30 @@ window.onload = () => {
       if (data.version === undefined) {
         data = {
           version: VERSION,
-          content: ''
+          content: '',
+          mode: 'day'
         };
         browser.storage.sync.set(data);
       }
+      else if (data.version === 1) {
+        browser.storage.sync.set({
+          version: VERSION,
+          mode: 'day'
+        });
+      }
 
       $textarea.value = data.content;
+    });
+
+    // Mode
+    browser.storage.sync.get().then( data => {
+      $mode_switcher.dataset.current = data.mode || "day";
+      if (data.mode == "night") {
+        $textarea.classList.add('dark');
+      }
+      else {
+        $textarea.classList.remove('dark');
+      }
     });
   };
   initNote();
@@ -27,4 +46,13 @@ window.onload = () => {
   // when actived window or tab changed
   browser.tabs.onActivated.addListener(initNote);
   browser.windows.onFocusChanged.addListener(initNote);
+
+  // day and night mode switcher
+  $mode_switcher.addEventListener('click', event => {
+    var isDay = $mode_switcher.dataset.current == 'day';
+    $mode_switcher.dataset.current = isDay ? 'night' : 'day';
+    $textarea.classList.toggle('dark');
+    browser.storage.sync.set({ mode: isDay ? 'night' : 'day' });
+  });
 };
+
