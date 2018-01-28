5a0c30644473bea3900750e7975dd829f8e8de9b
Nenad Stojanovikj <nekk1@live.com>
2018-01-28 13:02:06 +0100

    Add timeout on writing to storage, refactor file

--- newtab.js
+++ newtab.js
@@ -1,47 +1,49 @@
+const theme = {
+  night: "night",
+  day: "day"
+};
+
 window.onload = () => {
+  var writeTimeout;
   var $textarea = document.querySelector('#note-content');
   var $mode_switcher = document.querySelector('#mode-switcher');
 
   var initNote = () => {
     browser.storage.sync.get().then(data => {
-      if (data.content === undefined) {
-        data = {
-          content: '',
-          mode: 'day'
-        };
-      }
-
-      browser.storage.sync.set(data);
-      $textarea.value = data.content;
-    });
-
-    // Mode
-    browser.storage.sync.get().then(data => {
-      $mode_switcher.dataset.current = data.mode || "day";
-      if (data.mode == "night") {
+      // Check the theme
+      if (data.mode == theme.night) {
         $textarea.classList.add('dark');
-      }
-      else {
+      } else {
         $textarea.classList.remove('dark');
       }
+
+      // Update the page
+      $textarea.value = data.content || "";
+      $mode_switcher.dataset.current = data.mode || theme.day;
     });
   };
-  initNote();
+
+  var updateStorage = newData => {
+    browser.storage.sync.set({ content: newData });
+  };
 
   // when user writing
   $textarea.addEventListener('keyup', event => {
-    browser.storage.sync.set({ content: event.target.value });
+    clearTimeout(writeTimeout);
+    writeTimeout = setTimeout(() => updateStorage(event.target.value), 1000);
   });
-
-  // when actived window or tab changed
-  browser.tabs.onActivated.addListener(initNote);
-  browser.windows.onFocusChanged.addListener(initNote);
 
   // day and night mode switcher
   $mode_switcher.addEventListener('click', event => {
-    var isDay = $mode_switcher.dataset.current == 'day';
-    $mode_switcher.dataset.current = isDay ? 'night' : 'day';
+    var reverseMode = $mode_switcher.dataset.current == theme.day ? theme.night : theme.day;
     $textarea.classList.toggle('dark');
-    browser.storage.sync.set({ mode: isDay ? 'night' : 'day' });
+    $mode_switcher.dataset.current = reverseMode;
+    browser.storage.sync.set({ mode: reverseMode });
   });
+
+  // re-init when actived window or tab changed
+  browser.tabs.onActivated.addListener(initNote);
+  browser.windows.onFocusChanged.addListener(initNote);
+
+  initNote();
 };
